6ec7310ce2b2f1f7d28b32cfbe316ad125003d7f
Flameruin <flame.dekel@gmail.com>
2019-04-01 22:59:47 +0300

    progress

---
+++ js/app.js
@@ -0,0 +1,113 @@
+particlesJS('particles-js',
+  {
+    "particles": {
+      "number": {
+        "value": 159,
+        "density": {
+          "enable": true,
+          "value_area": 641.3648243462092
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "polygon",
+        "stroke": {
+          "width": 0,
+          "color": "#2f0c57"
+        },
+        "polygon": {
+          "nb_sides": 7
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.3472249623182,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 9.1764724990052,
+          "opacity_min": 1,
+          "sync": true
+        }
+      },
+      "size": {
+        "value": 5,
+        "random": true,
+        "anim": {
+          "enable": true,
+          "speed": 1,
+          "size_min": 0,
+          "sync": true
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 110.48066982851817,
+        "color": "#756c7f",
+        "opacity": 0.6155351604731727,
+        "width": 2.5252724532232724
+      },
+      "move": {
+        "enable": true,
+        "speed": 2,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 473.4885849793636,
+          "rotateY": 631.3181133058181
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 167.83216783216784,
+          "line_linked": {
+            "opacity": 0.056665938104605344
+          }
+        },
+        "bubble": {
+          "distance": 1500,
+          "size": 500,
+          "duration": 10,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 271.72827172827175,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  }
+
+);
